39ab66f0c7fd40d0f83bac92474880cf2892b4b1
micaelrosario <micaeldevrosario@gmail.com>
2025-11-04 20:47:38 -0300

    view Form Login

--- books_tech/models.py
+++ books_tech/models.py
@@ -10,3 +10,9 @@ class Post(models.Model):
         return self.titulo + ' | ' + str(self.autor)
 
 
+class Usuario(models.Model):
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
--- books_tech/views.py
+++ books_tech/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from .models import Usuario
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return render(request, 'home.html', {})
+    context = {
+        'user': Usuario.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def login(request):
+    return render(request, 'login.html')
+
+def fazerLogin(request):
+    user = authenticate(
+        username=request.POST['username'], 
+        password=request.POST['password'])
+    if user:
+        auth_login(request, user=user)
+    return HttpResponseRedirect(reverse('home'))
